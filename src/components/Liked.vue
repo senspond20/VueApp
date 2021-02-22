44e65b23365ca96f36b81a02ebc1a530842f2808
senspond20 <senspond@gmail.com>
2021-02-22 15:34:51 +0900

    todolist

---
+++ src/components/Liked.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class ="liked">
+        <div v-if="liked1">좋아요 눌렀음</div>
+        <button v-else v-on:click="onClickButton1">Like</button>
+
+        <div v-if="liked2">좋아요 눌렀음</div>
+        <button v-else v-on:click="onClickButton2">Like</button>
+        
+        <div v-if="liked3">좋아요 눌렀음</div>
+        <button v-else v-on:click="onClickButton3">Like</button>
+    </div>
+
+</template>
+
+<script>
+  const app = new Vue({
+  el : '#liked', // root를 Vue가 통제하고 있다.
+      data : {
+          liked1 : false, //liked 라는 데이터가 false
+          liked2 : false, 
+          liked3 : false, 
+      },
+      methods: {
+          onClickButton1(){
+              this.liked1 = true; //data.liked = true
+          },
+          onClickButton2(){
+              this.liked2 = true; //data.liked = true
+          },
+          onClickButton3(){
+              this.liked3 = true; //data.liked = true
+          }
+      }
+    });
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Example from '@/components/Example'
+import TodoPage from '@/components/TodoPage'
 
 Vue.use(Router)
 
@@ -11,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/example',
+      name: 'Example',
+      component: Example
+    },
+    {//추가
+      path: '/todos',
+      name: 'TodoPage',
+      component: TodoPage
     }
   ]
 })
